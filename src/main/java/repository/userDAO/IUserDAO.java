328b39b784c4e3ab79e02d8335200fb47bfaf49f
NTA1210 <anhntde180721@fpt.edu.vn>
2025-03-07 22:12:42 +0700

    NTA - CHECK

--- src/main/java/repository/userDAO/IUserDAO.java
+++ src/main/java/repository/userDAO/IUserDAO.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package repository.userDAO;
 
 
@@ -9,10 +6,6 @@ import repository.productDAO.*;
 import java.util.List;
 import model.Product;
 
-/**
- *
- * @author ASUS
- */
 public interface IUserDAO {
     public Product create(Product user);
     public Product findById(int id);
