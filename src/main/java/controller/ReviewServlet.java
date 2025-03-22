b9fb735e76e0b514fdadf14581b7b4dc96d2fd60
NTA1210 <anhntde180721@fpt.edu.vn>
2025-03-23 01:52:08 +0700

    check loi

--- src/main/java/controller/ReviewServlet.java
+++ src/main/java/controller/ReviewServlet.java
@@ -7,14 +7,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
-import static java.lang.System.out;
 import java.util.Date;
 import java.util.List;
 import model.Product;
 import model.Review;
 import model.User;
-import orderDAO.OrderDAO;
 import productDAO.ProductDAO;
 import reviewDAO.ReviewDAO;
 
